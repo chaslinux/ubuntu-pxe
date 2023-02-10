cf370ddabe23f3d0b760d4de5d674feffa30566e
Chaslinux <97259120+chaslinux@users.noreply.github.com>
2023-02-10 16:32:38 -0500

    Create ubuntu-pxe.sh
    Initial unfinished commit

---
+++ ubuntu-pxe.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Attempt to automate the setup of a PXE server as documented on Ubuntu Discourse forums
+# https://discourse.ubuntu.com/t/netbooting-the-server-installer-on-amd64/16620
+
+# By Chaslinux, chaslinux@gmail.com
+# I use pfsense rather than dnsmasq for DNS, so skipping that section
+
+JAMMYSERVER=jammy-live-server-amd64.iso
+
+# Make the tftp directory to hold Ubuntu Jammy Server
+sudo mkdir -p /srv/tftp/ubuntu/jammy/server
+
+# instructions don't use sudo, how are they doing this?
+# get the boot images
+sudo apt install cd-boot-images-amd64
+sudo ln -s /usr/share/cd-boot-images-amd64 /srv/tftp/boot-amd64
+
+# Get Ubuntu 22.04 Jammy Server ISO
+cd ~
+wget http://cdimage.ubuntu.com/ubuntu-server/jammy/daily-live/current/$JAMMYSERVER
+sudo mount $JAMMYSERVER /mnt
+sudo cp /mnt/casper/{vmlinuz,initrd} /srv/tftp/ubuntu/jammy/server
+
+# set up files for UEFI booting
+# note: sudo doesn't appear to be needed for this first part
+apt download shim-signed
+dpkg-deb --fsys-tarfile shim-signed*deb | tar x ./usr/lib/shim/shimx64.efi.signed -O > bootx64.efi
+sudo mv bootx64.efi /srv/tftp
